d1f843ab222ed1f7c515bc31fdad14dda7b7dc9d
Andrii Butenko <andrejbutik@gmail.com>
2025-05-13 18:09:24 +0200

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -3,7 +3,7 @@ registerForm.addEventListener('submit', event => {
     event.preventDefault();
     const form = event.target;
     const login = form.elements.email.value;
-    const password = form.elements.password.value;
+    const password = form.elements.password.value.trim();
       if (login === "" || password === "") {
     return alert('All form fields must be filled in');
     };
